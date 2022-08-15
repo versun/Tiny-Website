4857dd227663ccab3962b53649a8cd59e2bf5314
Versun <me+github7604@versun.org>
2022-08-15 07:19:39 +0000

    fix amplenote darkmode

--- scripts/global.js
+++ scripts/global.js
@@ -1,34 +1,39 @@
 var darkMode = window.localStorage.getItem('darkMode');
-if(!darkMode){
-	darkMode =  false;
+if (!darkMode) {
+  darkMode = false;
 }
-else if(darkMode === "true"){
-	darkMode = true;
+else if (darkMode === "true") {
+  darkMode = true;
 }
-else{
-	darkMode = false;
+else {
+  darkMode = false;
 }
 mode();
 
 
-function darkToggle(){
-	toggle();
-	mode();
+function darkToggle() {
+  toggle();
+  mode();
 }
 
-function mode(){
-	var button = document.getElementById("dark-button");
-	if(darkMode || darkMode){
-		document.body.classList.add("dark");
-		button.innerText = "Light Mode";
-	}
-	else{
-		document.body.classList.remove("dark");
-		button.innerText = "Dark Mode";
-	}
+function mode() {
+  var button = document.getElementById("dark-button");
+  var amplenote = document.getElementsByClassName("amplenote-embed")[0];
+  if (darkMode || darkMode) {
+    document.body.classList.add("dark");
+    amplenote.setAttribute("data-styles", "/css/amplenote-dark.css");
+    document.querySelector("iframe").contentWindow.location.reload(true);
+    button.innerText = "Light Mode";
+  }
+  else {
+    document.body.classList.remove("dark");
+    amplenote.setAttribute("data-styles", "/css/amplenote.css");
+    document.querySelector("iframe").contentWindow.location.reload(true);
+    button.innerText = "Dark Mode";
+  }
 }
 
-function toggle(){
-	darkMode = !darkMode;
-	window.localStorage.setItem('darkMode',darkMode);	
+function toggle() {
+  darkMode = !darkMode;
+  window.localStorage.setItem('darkMode', darkMode);
 }
